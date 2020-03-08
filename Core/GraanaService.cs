1bb6aca6e61ab93428285ec2f0ddead4d7828444
ameerhamza0334 <ameerhamza0334@gmail.com>
2020-03-09 01:39:31 +0500

    Add files via upload

---
+++ Core/GraanaService.cs
@@ -0,0 +1,29 @@
+﻿
+using Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    public class GraanaService
+    {
+
+        private readonly TrainingEntities _db;
+
+
+        public GraanaService()
+        {
+            _db = new TrainingEntities();
+        }
+
+        public List<SearchByString_Result> SearchByEveryThing(string SearchString)
+        {
+            List<SearchByString_Result> result = _db.SearchByString(SearchString).ToList();
+            
+            return result;
+        }
+    }
+}
